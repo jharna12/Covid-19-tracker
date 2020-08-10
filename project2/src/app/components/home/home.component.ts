b710444ee2bd68881e300c87465822b591072c96
jharna agrawal <jharna.agrawal_cs16@gla.ac.in>
2020-08-10 16:07:03 +0530

    calculation done

--- project2/src/app/components/home/home.component.ts
+++ project2/src/app/components/home/home.component.ts
@@ -1,18 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { DataServiceService } from '../../services/data-service.service';
+import {GlobalDataSummary} from '../../models/global-data';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+globalData: GlobalDataSummary[];
+totalConfirmed=0;
+totalActive=0;
+totalRecovered=0;
+totalDeath=0;
   constructor(private dataService : DataServiceService) { }
 
   ngOnInit(): void {
-    this.dataService.getGlobalData().subscribe(
-    result=>{
+    this.dataService.getGlobalData().
+    subscribe(
+  result=>{
         console.log(result);
+        this.globalData=result;
+
+        result.forEach(cs => {
+          if(Number.isNaN(cs.confirmed))
+          this.totalActive+=cs.active;
+          this.totalConfirmed+=cs.confirmed;
+          this.totalDeath+=cs.death;
+          this.totalRecovered+=cs.recovered;
+          
+        });
       })
     }
   }
--- project2/src/app/services/data-service.service.ts
+++ project2/src/app/services/data-service.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClientModule, HttpClient} from '@angular/common/http';
+import {GlobalDataSummary} from '../models/global-data';
 import{map} from 'rxjs/operators';
+import { templateJitUrl } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +13,55 @@ private globalDataUrl="https://raw.githubusercontent.com/CSSEGISandData/COVID-19
   constructor(private http:HttpClient) { }
 getGlobalData()
 {
+
   return this.http.get(this.globalDataUrl,{responseType:'text'}).pipe(
     map(result=>{
-  return result;
+      // to merge all the values into a single  and remove redundancy
+     // create a object  of type globalDataSummary  for assign and store  a key value pair 
+    
+     
+      let data : GlobalDataSummary[]=[];
+      let raw={} 
+      let row={}
+      let rows=result.split("\n");
+      rows.splice(0,1); // to remove header row and start the array from 1 index
+      rows.forEach(row=>{
+        let cols=row.split(/,(?=\S)/) // this regular expression is to consider all the names which are seperated with comma as a single name
+
+
+
+        // create a object for globaldatasummary
+       let  cs={
+        country : cols[3],
+        confirmed : +cols[7],
+        deaths : +cols[8],
+        recovered : +cols[9],
+        active : +cols[10] //  + to convert the data into number
+        }
+        let temp:GlobalDataSummary = raw[cs.country];
+        if(temp)
+        {
+          temp.confirmed=temp.confirmed+cs.confirmed;
+          temp.active=temp.active+cs.active;
+          temp.deaths=temp.deaths+cs.deaths;
+          temp.recovered=temp.recovered+cs.recovered;
+          raw[cs.country]=temp;
+        }
+        else
+         raw[cs.country]=cs;
+       
+
+
+
+// data.push({
+ 
+// })
+      //  console.log(cols);
+       // console.log(data);
+      })
+      // console.log (rows);
+  return <GlobalDataSummary[]>Object.values(raw);// to return only values not key
+  // typecast the value to globalData summary
 }
       ))
 
