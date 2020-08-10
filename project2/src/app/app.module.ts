b4c277f09779655cd0acee7b8ab33533566eecad
jharna agrawal <jharna.agrawal_cs16@gla.ac.in>
2020-08-10 13:55:08 +0530

    dataset added

--- project2/src/app/app.module.ts
+++ project2/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { CountriesComponent } from './components/countries/countries.component';
+import {HttpClientModule} from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CountriesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ project2/src/app/services/data-service.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+import{map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataServiceService {
+private globalDataUrl="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/04-05-2020.csv";
+
+  constructor(private http:HttpClient) { }
+getGlobalData()
+{
+  return this.http.get(this.globalDataUrl,{responseType:'text'}).pipe(
+    map(result=>{
+  return result;
+}
+      ))
+
+  }
+  }
